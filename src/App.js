1654704a1bd89fe373f5d78f8b8171a865bb973a
Jonathan Gucciardi <jonmgucciardi@Jonathans-MacBook-Pro.local>
2017-04-17 20:55:16 -0500

    test

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+// add seven additional things to this ract app
+
+
 function App() {
   return (
     <div className="App">
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to React</h2>
+        <p> Test Change </p>
       </div>
       <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
