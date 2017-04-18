3825a1863b96e629ed4c717b437db792c24147c2
Nick Rodriguez <Nick Rodriguez>
2017-04-17 20:32:58 -0500

    first update

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to React</h2>
+        <p>Test update</p>
       </div>
       <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
