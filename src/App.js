d749e9dd2fe50ce8d577092a196925d897890d18
saf0911 <saf0911@gmail.com>
2017-04-17 22:43:03 -0500

    first app

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "./App.css";
 
 function App() {
   return (
+
     <div className="App">
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to React</h2>
+        <marquee> Welcome back Mr Messier</marquee>
       </div>
       <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
