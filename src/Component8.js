8c0f3a32f90ff95e697f1f416f818eae478d10ed
RX7RUSS <russ.savage5@gmail.com>
2017-04-19 18:35:44 -0500

    Added Components

---
+++ src/Component8.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default class Component8 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Component8</h1>
+      </div>
+    );
+  }
+}
