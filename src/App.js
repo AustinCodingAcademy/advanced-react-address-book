2e003b828ff46db75360c41d2ba86840d50ea5be
saf0911 <saf0911@gmail.com>
2017-04-18 19:50:58 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to React</h2>
-        <marquee> Welcome back Mr Messier</marquee>
+        <marquee> Welcome back Mr Messnier</marquee>
       </div>
       <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
