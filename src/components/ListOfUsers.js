26645a6be5c5aee59002748974c2889aaabd1266
MicahMonfrini <ffrock905@gmail.com>
2017-04-28 22:29:04 -0500

    toggle userlist complete

--- src/components/ListOfUsers.js
+++ src/components/ListOfUsers.js
@@ -1,26 +1,42 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
-import Button from "./Button";
 
 class ListOfUsers extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      hideButtonWasClicked: false,
+    };
+  }
+
+  handleClick() {
+    this.setState({
+      hideButtonWasClicked: !this.state.hideButtonWasClicked
+    });
   }
 
   render() {
     const userList = this.props.users.map((user, index) => {
-      return (
-        <ul key={index}>
-          <li>
-            {user.firstName} {user.lastName}
-          </li>
-        </ul>
-      );
+      if (!this.state.hideButtonWasClicked) {
+        return (
+          <ul key={index}>
+            <li>
+              {user.firstName} {user.lastName}
+            </li>
+          </ul>
+        );
+      } else {
+        return null;
+      }
     });
+
     return (
       <div>
         {userList}
-        <Button />
+        <button
+          onClick={this.handleClick.bind(this)}>
+          {this.state.hideButtonWasClicked ? "Show" : "Hide"}
+        </button>
       </div>
     );
   }
