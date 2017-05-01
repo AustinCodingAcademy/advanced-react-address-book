f4097519c16daa90e455a9ba1c1f963124cfdac3
saf0911 <saf0911@gmail.com>
2017-05-01 12:12:25 -0500

    testing something

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ function App(props) {
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to React</h2>
       </div>
-      <AddH3 />
+      //<AddH3 />
       <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
