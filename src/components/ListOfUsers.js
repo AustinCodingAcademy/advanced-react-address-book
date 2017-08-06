094a43c0019556c3b7f2b489d4fd1ef67f4e237a
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-08-06 16:01:40 -0500

    almost done

---
+++ src/components/ListOfUsers.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+class ListOfUsers extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      visible: true,
+      searchText: "",
+    }
+  }
+
+  handleClickBtn = () => {
+    this.setState(prevState => {
+      return {
+        visible : !prevState.visible,
+      }
+    });
+  }
+  handleSearchBox = (event) => {
+    this.setState({
+      searchText : event.target.value,
+    });
+  }
+  render() {
+    // console.log(this.state.searchText);
+    const userDivs = this.props.users.map(user => {
+      if(this.state.visible) {
+        return <div key={user.id}>{user.first_name}</div>
+      }
+    });
+
+
+    return (
+      <div>
+        <input label="Search" autoFocus onChange={this.handleSearchBox}/>
+        {userDivs}
+        <button onClick={this.handleClickBtn}>
+          {this.state.visible ? "Hide" : "Show"}
+        </button>
+      </div>
+    );
+  }
+}
+
+export default ListOfUsers;
